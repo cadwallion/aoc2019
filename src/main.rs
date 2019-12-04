cd95e1dc7b355b0aa48f2e28b313fb9caeb2e1ef
Andrew Nordman <cadwallion@github.com>
2019-12-04 10:13:35 -0800

    It's 1a and 1b, silly

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn day1() {
+fn day1a() {
     let file = File::open("./inputs/day1.txt").unwrap();
     let reader = BufReader::new(file);
     let mut sum = 0.0;
     for (_, line) in reader.lines().enumerate() {
         sum += get_fuel_cost(line.unwrap().parse::<f64>().unwrap());
     }
-    println!("Day 1: {}", sum);
+    println!("Day 1a: {}", sum);
 }
 
 fn get_fuel_cost(mass: f64) -> f64 {
@@ -16,7 +16,7 @@ fn get_fuel_cost(mass: f64) -> f64 {
 }
 
 // 4841054
-fn day2() {
+fn day1b() {
     let file = File::open("./inputs/day1.txt").unwrap();
     let reader = BufReader::new(file);
     let mut sum = 0.0;
@@ -28,10 +28,10 @@ fn day2() {
             mass = get_fuel_cost(mass);
         }
     }
-    println!("Day 2: {}", sum);
+    println!("Day 1b: {}", sum);
 }
 
 fn main() {
-    day1();
-    day2();
+    day1a();
+    day1b();
 }
