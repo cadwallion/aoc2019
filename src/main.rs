aa1ad80c334df6ec996a5d11381385e3009f9433
Andrew Nordman <cadwallion@github.com>
2019-12-03 17:34:51 -0800

    Day2

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,32 @@ fn day1() {
     let reader = BufReader::new(file);
     let mut sum = 0.0;
     for (_, line) in reader.lines().enumerate() {
-        let mass: f64 = line.unwrap().parse::<f64>().unwrap();
-        let value = ((mass / 3.0).floor()) - 2.0;
-        sum += value;
+        sum += get_fuel_cost(line.unwrap().parse::<f64>().unwrap());
     }
-    println!("The sum is {}", sum);
+    println!("Day 1: {}", sum);
+}
+
+fn get_fuel_cost(mass: f64) -> f64 {
+    ((mass / 3.0).floor()) - 2.0
+}
+
+// 4841054
+fn day2() {
+    let file = File::open("./inputs/day1.txt").unwrap();
+    let reader = BufReader::new(file);
+    let mut sum = 0.0;
+    for (_, line) in reader.lines().enumerate() {
+        let mut mass = line.unwrap().parse::<f64>().unwrap();
+        mass = get_fuel_cost(mass);
+        while mass > 0.0 {
+            sum += mass;
+            mass = get_fuel_cost(mass);
+        }
+    }
+    println!("Day 2: {}", sum);
 }
 
 fn main() {
     day1();
+    day2();
 }
